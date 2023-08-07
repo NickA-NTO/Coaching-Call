6f6c16b35f0c93cfac0779508716f04e9182ad7d
NickA-NTO <139092914+NickA-NTO@users.noreply.github.com>
2023-08-06 22:37:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,40 +22,14 @@ app.post('/webhook', (req, res) => {
   console.log(req.headers)
   console.log(req.body)
 
-  // construct the message string
   const message = `v0:${req.headers['x-zm-request-timestamp']}:${JSON.stringify(req.body)}`
-
   const hashForVerify = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(message).digest('hex')
-
-  // hash the message string with your Webhook Secret Token and prepend the version semantic
   const signature = `v0=${hashForVerify}`
 
-  // you validating the request came from Zoom https://marketplace.zoom.us/docs/api-reference/webhook-reference#notification-structure
-  if (req.headers['x-zm-signature'] === signature) {
-
-    // Zoom validating you control the webhook endpoint https://marketplace.zoom.us/docs/api-reference/webhook-reference#validate-webhook-endpoint
-    if(req.body.event === 'endpoint.url_validation') {
-      const hashForValidate = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(req.body.payload.plainToken).digest('hex')
-
-      response = {
-        message: {
-          plainToken: req.body.payload.plainToken,
-          encryptedToken: hashForValidate
-        },
-        status: 200
-      }
-
-      console.log(response.message)
-
-      res.status(response.status)
-      res.json(response.message)
-    } else {
-      response = { message: 'Authorized request to Zoom Webhook sample.', status: 200 }
-
-      console.log(response.message)
-
-      res.status(response.status)
-      res.json(response)
+  if (req.headers['x-zm-signature'] !== signature) {
+    console.log('Unauthorized request to Zoom Webhook.')
+    return res.status(403).end()
+  }
 
   if(req.body.event === 'meeting.participant_joined') {
     const participantName = req.body.payload.object.participant.user_name;
