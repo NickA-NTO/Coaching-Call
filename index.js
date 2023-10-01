32b2fa65df22bc8143622b1084202a67d1dd5c0a
NickA-NTO <139092914+NickA-NTO@users.noreply.github.com>
2023-10-01 14:47:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -64,36 +64,12 @@ app.post('/webhook', (req, res) => {
     // List of meeting IDs you're interested in
     const targetMeetingIds = [
       '7214926104','3401482925','2173325443','7873022402','5257477503','9522960235','6872746626','3075618023','5519524161','9223375392','8504100770','4320805963','7614622031','3825701420','8411939930','8465883832','8020338961','6263281468','8611941283','5203069113','9029618114','6819765727','5642208590','6461130412','5336564866','3400768558','6396767386','8967202011','4774066127','9998173534','7669829455','2739038613','9681127256','2725784985','3776271342','7250159365','9072235292','5103702513','8917770397','3807441029','4825464477','5615261244','5301098967','5117169595','7363344485','8686142782','7848749909','6030598690','3569538459','2943012912','4835843786','5088144791','6446311624','9773038851','7529538283','8230366299','5944978371','4541815666','8240787316','4780763670',
-      '89120883983'
-      ]
+      '89120883983','87294708840','89947112674','83654859073','82528624597','85888215571','85160137284','86347667426','89298967434','83755285840','82632853218','82907100240','89031219096','86235587288'
     ];
   
     // List of participant names to ignore
     const ignoredParticipants = [
-      'Vidhi Vashishth',
-      'Ruchi Baid',
-      'Otavio Rocha',
-      'Omolara Salawu',
-      'Mallika Alai',
-      'Laura Mejia',
-      'Lakshmy Sobha',
-      'Karina Jimenez',
-      'Joshua Albar',
-      'Joao Pinto',
-      'Javier Rodriguez',
-      'Ilma Cohadzic',
-      'Himanshi Goyal',
-      'Haannie Kazmi',
-      'Filipe Barcellos',
-      'David Babagbale',
-      'Daniela Guarin',
-      'Bruna Rodrigues',
-      'Brenno Peserico',
-      'Audri Wolfaardt',
-      'Arslan Imtiaz',
-      'Airic Carrillo',
-      'Raditya Dwiprasta',
-      'Rio Purnomo'
+    'Vidhi Vashishth','Ruchi Baid','Otavio Rocha','Omolara Salawu','Mallika Alai','Laura Mejia','Lakshmy Sobha','Karina Jimenez','Joshua Albar','Joao Pinto','Javier Rodriguez','Ilma Cohadzic','Himanshi Goyal','Haannie Kazmi','Filipe Barcellos','David Babagbale','Daniela Guarin','Bruna Rodrigues','Audri Wolfaardt','Arslan Imtiaz','Airic Carrillo','Raditya Dwiprasta','Rio Purnomo','Awais Akbar','Nishal Nandwani'
     ];
   
     if (!targetMeetingIds.includes(meetingId) || ignoredParticipants.includes(participantName)) {
